6d6fd0191044ed21268838b748a5321c811f2f2d
Alexander Barton <alex@barton.de>
2014-02-10 21:50:28 +0100

    Add new mirror: ngircd.mirror.3rz.org
    Many thanks to Götz Hoffart!

--- mirrors.php
+++ mirrors.php
@@ -6,11 +6,11 @@
 	Mirrors / Spiegel
 </h2>
 <ul>
-	<li><a href="http://ngircd.berlios.de/">ngircd.berlios.de</a>
 	<li><a href="http://ngircd.barton.de/">ngircd.barton.de</a>
+	<li><a href="http://ngircd.mirror.3rz.org">ngircd.mirror.3rz.org</a>
+	<li><a href="http://ngircd.berlios.de/">ngircd.berlios.de</a>
 </ul>
 
 <?php
 	end_page();
 ?>
-

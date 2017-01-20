bc0f65a5e025e8fd094134dce4dff2d1f1b8a79c
Alexander Barton <alex@barton.de>
2017-01-20 21:46:58 +0100

    ngircd.barton.de uses HTTPS!

--- mirrors.php
+++ mirrors.php
@@ -6,7 +6,7 @@
 	Mirrors / Spiegel
 </h2>
 <ul>
-	<li><a href="http://ngircd.barton.de/">ngircd.barton.de</a>
+	<li><a href="https://ngircd.barton.de/">ngircd.barton.de</a>
 	<li><a href="https://ngircd.mirror.3rz.org">ngircd.mirror.3rz.org</a>
 </ul>
 

92db9e1fe3f6827b3d52af1b980b3ca0f4896b96
Alexander Barton <alex@barton.de>
2019-01-24 01:07:00 +0100

    Remove ngircd.mirror.3rz.org, add ngircd.sourceforge.net mirror

--- mirrors.php
+++ mirrors.php
@@ -6,8 +6,11 @@
 	Mirrors / Spiegel
 </h2>
 <ul>
-	<li><a href="https://ngircd.barton.de/">ngircd.barton.de</a>
-	<li><a href="https://ngircd.mirror.3rz.org">ngircd.mirror.3rz.org</a>
+	<li><a href="https://ngircd.barton.de/">ngircd.barton.de</a>&thinsp;🔒
+</ul>
+<ul>
+	<li><a href="https://ngircd.mirror.3rz.org">ngircd.mirror.3rz.org</a>&thinsp;🔒
+	<li><a href="http://ngircd.sourceforge.net/">ngircd.sourceforge.net</a>
 </ul>
 
 <?php

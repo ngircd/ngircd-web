616ed0a94a2b79712c92aa260ac8ed2f9be42e6e
Alexander Barton <alex@barton.de>
2020-01-01 23:50:55 +0100

    Enable new mirror: ngircd.mirror.3rz.de

--- mirrors.php
+++ mirrors.php
@@ -9,6 +9,7 @@
 	<li><a href="https://ngircd.barton.de/">ngircd.barton.de</a>&thinsp;🔒
 </ul>
 <ul>
+	<li><a href="https://ngircd.mirror.3rz.de/">ngircd.mirror.3rz.de</a>&thinsp;🔒
 	<li><a href="https://ngircd.sourceforge.io/">ngircd.sourceforge.io</a>&thinsp;🔒
 </ul>
 
